40b6545c624756c8f99a7115da88b8c5d219d495
Andrew Kim <andrewikim95@gmail.com>
2021-03-15 14:19:47 -0400

    restarting the skeleton

--- CharmTunes/db/migrate/20210309060611_create_artist.rb
+++
@@ -1,9 +0,0 @@
-class CreateArtist < ActiveRecord::Migration[5.2]
-  def change
-    create_table :artists do |t|
-      t.integer :song_id
-      t.integer :album_id
-      t.string 
-    end
-  end
-end
--- CharmTunes/frontend/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-
-document.addEventListener("DOMContentLoaded", () => {
-    
-    ReactDOM.render(<h1>Hello!</h1>, document.getElementById("root"))
-}) ;
