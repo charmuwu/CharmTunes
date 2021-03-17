8cd0180a1ae401d1b4f8ed0570aef3fbd3320b3e
Andrew Kim <andrewikim95@gmail.com>
2021-03-17 17:32:55 -0400

    for heroku push

--- config/application.rb
+++ config/application.rb
@@ -5,10 +5,11 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
 module CharmTunes
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.assets.js_compressor = Uglifier.new(harmony: true)
+    config.assets.initialize_on_precompile = false
     config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
