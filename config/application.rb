b2cec8757d244c20192775532f9efeb53c9d9c03
Andrew Kim <andrewikim95@gmail.com>
2021-03-17 17:39:01 -0400

    for heroku push

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require_relative 'boot'
 
 require 'rails/all'
 
-config.assets.js_compressor = Uglifier.new(harmony: true)
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
