d1534694c9a8cdda1bf0392148fed973c96943ab
Andrew Kim <andrewikim95@gmail.com>
2021-03-18 16:53:30 -0400

    added in icon

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -5,7 +5,7 @@ import {AuthRoute, ProtectedRoute} from '../utils/route_utils'
 
 const App = () => (
   <div>
-    <h1>imagine not having this finished yet</h1>
+    
     
     <AuthRoute path="/signup" component={SignUpContainer} />
     
--- frontend/components/sessions/signup_form.jsx
+++ frontend/components/sessions/signup_form.jsx
@@ -20,46 +20,46 @@ class SignUpForm extends React.Component {
     render() {
       return (
         <div>
-          <h1 id="formheader">Sign up for free to start listening.</h1>
-
-          <br/>
-          <h3 id="formtitle">Sign up with your email address</h3>
-          <form onSubmit={this.handleSubmit} className="signupform">
-            <label id="formlabel" htmlFor="email">
-                What's your email?
-            </label>
-            <input
-                id="email"
-                className="input"
-                type='text'
-                placeholder="Enter your email."
-                value={this.state.email}
-                onChange={this.update('email')}
-            />
-            <label id="formlabel" htmlFor="username">
-                Create a username
-            </label>
-            <input
-                id="username"
-                className="input"
-                type='text'
-                placeholder="Create a username."
-                value={this.state.username}
-                onChange={this.update('username')}
-            />
-            <label id="formlabel" htmlFor="password">
-                Create a password
-            </label>
-            <input
-                id="password"
-                className="input"
-                type='password'
-                placeholder="Create a password."
-                value={this.state.password}
-                onChange={this.update('password')}
-            />
-            <button type='submit' className="signupbutton">SIGN UP</button>
-          </form>
+            <div className="icon"></div>
+            <h1 id="formheader">Sign up for free to start listening.</h1>
+            <br/>
+            <h3 id="formtitle">Sign up with your email address</h3>
+            <form onSubmit={this.handleSubmit} className="signupform">
+                <label id="formlabel" htmlFor="email">
+                    What's your email?
+                </label>
+                <input
+                    id="email"
+                    className="input"
+                    type='text'
+                    placeholder="Enter your email."
+                    value={this.state.email}
+                    onChange={this.update('email')}
+                />
+                <label id="formlabel" htmlFor="username">
+                    Create a username
+                </label>
+                <input
+                    id="username"
+                    className="input"
+                    type='text'
+                    placeholder="Create a username."
+                    value={this.state.username}
+                    onChange={this.update('username')}
+                />
+                <label id="formlabel" htmlFor="password">
+                    Create a password
+                </label>
+                <input
+                    id="password"
+                    className="input"
+                    type='password'
+                    placeholder="Create a password."
+                    value={this.state.password}
+                    onChange={this.update('password')}
+                />
+                <button type='submit' className="signupbutton">SIGN UP</button>
+            </form>
         </div>
       );
     }
