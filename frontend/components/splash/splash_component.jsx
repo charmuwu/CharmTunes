d743ec3f11979edf7a604e6273b22d4ec023a108
Andrew Kim <andrewikim95@gmail.com>
2021-03-23 01:11:24 -0400

    added a triangle to nav profile button

--- frontend/components/splash/splash_component.jsx
+++ frontend/components/splash/splash_component.jsx
@@ -9,10 +9,19 @@ class SplashComponent extends React.Component{
         function btnclickdrop(){ 
             const dropdwn = document.getElementById("drpdwnmenu");
             dropdwn.classList.toggle("show");
+
+            const tri = document.getElementById("triangle");
+            tri.classList.remove('triangle');
+            tri.classList.add('down');
+
             window.onclick = function(e) {
                 if (!e.target.matches('.profilebtn')) {  
                     if (dropdwn.classList.contains('show')) {
                         dropdwn.classList.remove('show');
+                    }
+                    if (tri.classList.contains('.down')){
+                        tri.classList.remove('down');
+                        tri.classList.add('triangle');
                     }
                 }
             }
@@ -25,9 +34,12 @@ class SplashComponent extends React.Component{
         )
         const ifLoggedIn = () => (
             <div className="splashbuttons">
-                <button className="profilebtn" onClick={btnclickdrop}>
-                    {this.props.currentUser.username}
-                </button>
+                <div>
+                    <button className="profilebtn" onClick={btnclickdrop}>
+                        {this.props.currentUser.username}
+                    </button>
+                    <div  id="triangle"></div>
+                </div>
                 <div className="dropdownmenu" id="drpdwnmenu">
                     <Link className="profile" to={`/users/${this.props.currentUser.id}`} >Profile</Link>
                     <input className="splashlogout" type="submit" onClick={this.props.logout} value="Log Out"/>
