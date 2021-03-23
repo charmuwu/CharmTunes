14c567cfe9f765224c1daf80b3c3e5d3a3083b84
Andrew Kim <andrewikim95@gmail.com>
2021-03-23 00:46:19 -0400

    added in pause play mute buttons

--- frontend/components/musicplayer/music_component.jsx
+++ frontend/components/musicplayer/music_component.jsx
@@ -4,12 +4,38 @@ import { Link } from 'react-router-dom';
 class MusicComponent extends React.Component {
     constructor(props){
         super(props)
-        
+        this.audioRef = React.createRef();
+        this.state = {
+            playing: false,
+            volume: 0.5,
+        }
+        // this.handleVolume = this.handleVolume.bind(this);
+        this.handleMute = this.handleMute.bind(this);
+        this.handlePlay = this.handlePlay.bind(this);
     }
     componentDidMount(){
         this.props.getSongs();
     }
-
+    handlePlay(){
+        if(this.state.playing){
+            this.audioRef.current.pause();
+            this.setState({playing: false});
+        } else{
+            this.audioRef.current.play();
+            this.setState({playing: true});
+        }
+    }
+    handleMute(){
+        if(this.audioRef.current.muted){
+            this.audioRef.current.muted = false; //unmutes
+            // this.setState({}) //brings back to
+        } else {
+            this.audioRef.current.muted = true; //mutes
+        }
+    }
+    handleVolume(){
+        return e => this.setState({ volume: e.currentTarget.value });
+    }
     render() {
         const ifLoggedOut = () => (
             <div className="musicplayerdiv" id="bottomsignupsplash">
@@ -22,7 +48,23 @@ class MusicComponent extends React.Component {
         )
         const ifLoggedIn = () => (
             <div className="musicplayerdiv">
-                <audio controls className="musicplayer">
+                <div>
+                    <button className="play" onClick={this.handlePlay}>play pause</button>
+                </div>
+                <div>
+                    <button className="mute" onClick={this.handleMute}>mute</button>
+                </div>
+                <div>
+                    <input 
+                        type="range" 
+                        className="volume"
+                        min="1"
+                        max="100" 
+                        value="100"
+                        onChange={this.handleVolume}> 
+                    </input>
+                </div>
+                <audio className="musicplayer" ref={this.audioRef}>
                     <source src={window.mp3url} type="audio/mpeg"/>
                 </audio>
                 <p></p>
--- frontend/components/musicplayer/music_container.jsx
+++ frontend/components/musicplayer/music_container.jsx
@@ -3,7 +3,6 @@ import MusicComponent from './music_component';
 import {getSong, getSongs} from '../../actions/music_actions';
 
 const mSTP = state => {
-    debugger
     return{
     currentUser: state.entities.users[state.session.currentUser],
     songs: state.entities.songs
