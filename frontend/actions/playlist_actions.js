72fb7336e4ac7d005521dddd3bf2c4ce4fbe71bd
Andrew Kim <andrewikim95@gmail.com>
2021-10-19 15:34:23 -0400

    add playlist action

--- frontend/actions/playlist_actions.js
+++ frontend/actions/playlist_actions.js
@@ -2,6 +2,7 @@ import {fetchPlaylists, fetchPlaylist, postPlaylist, deletePlaylist} from '../ut
 
 export const RECEIVE_CURRENT_PLAYLIST = 'RECEIVE_CURRENT_PLAYLIST';
 export const RECEIVE_PLAYLISTS = 'RECEIVE_PLAYLISTS';
+export const DELETE_PLAYLIST = 'DELETE_PLAYLIST';
 
 export const getPlaylist = playlistId => dispatch => fetchPlaylist(playlistId)
     .then( playlist => dispatch({type: RECEIVE_CURRENT_PLAYLIST, playlist}));
@@ -13,4 +14,8 @@ export const newPlaylist = formPlaylist => dispatch => postPlaylist(formPlaylist
     .then((playlist) => dispatch({
        type: RECEIVE_CURRENT_PLAYLIST,
        playlist}), error => dispatch(receiveErrors(error.responseJSON)));
+
+export const noMorePlaylist = () => dispatch => deletePlaylist()
+    .then(() => dispatch({
+    type: DELETE_PLAYLIST}));
 
