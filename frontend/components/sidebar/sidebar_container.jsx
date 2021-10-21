3bca907a020e28783cf56af630901e2e5a2c130e
Andrew Kim <andrewikim95@gmail.com>
2021-10-21 15:14:27 -0400

    add mSTP mDTP to sidebar container

--- frontend/components/sidebar/sidebar_container.jsx
+++ frontend/components/sidebar/sidebar_container.jsx
@@ -1,5 +1,17 @@
 import { connect } from 'react-redux';
 import SidebarComponent from './sidebar_component';
+import { fetchPlaylist, fetchPlaylists, postPlaylist, deletePlaylist } from '../../utils/playlist_api_util';
 
+const mSTP = state => {
+    return{
+        playlist: state.playlist
+    }
+};
+const mDTP = dispatch => ({
+    getPlaylist: playlistId => dispatch(fetchPlaylist(playlistId)),
+    getPlaylists: () => dispatch(fetchPlaylists()),
+    newPlaylist: playlist => dispatch(postPlaylist(playlist))
 
-export default connect(null,null)(SidebarComponent)
+});
+
+export default connect(mSTP,mDTP)(SidebarComponent)
