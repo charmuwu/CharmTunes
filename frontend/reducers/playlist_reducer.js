0813057ced59e16b17fea713e08e19cc16acda0b
Andrew Kim <andrewikim95@gmail.com>
2021-10-23 23:39:08 -0400

    move sidebar icon left down

--- frontend/reducers/playlist_reducer.js
+++ frontend/reducers/playlist_reducer.js
@@ -1,10 +1,12 @@
-import { RECEIVE_PLAYLIST } from '../actions/playlist_actions';
+import { RECEIVE_PLAYLIST, RECEIVE_PLAYLISTS } from '../actions/playlist_actions';
 
 const playlistReducer = (state={}, action) => {
     Object.freeze(state);
     switch(action.type){
         case RECEIVE_PLAYLIST:
             return action.playlist;
+        case RECEIVE_PLAYLISTS:
+            return action.playlists;
         default:
             return state;
     }
