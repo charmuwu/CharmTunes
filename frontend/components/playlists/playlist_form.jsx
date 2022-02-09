a9f94134a92bb46de198289482b76bde61668b5d
Andrew Kim <andrewikim95@gmail.com>
2022-02-09 08:00:46 -0500

    playlist form styling update

--- frontend/components/playlists/playlist_form.jsx
+++ frontend/components/playlists/playlist_form.jsx
@@ -40,7 +40,7 @@ class PlaylistForm extends React.Component {
             <div className="playlistContainer">
 
                 <div className="plinfobar">
-                    <div className="plartwork">
+                    <div className="plartwork" onClick={this.handlePLClickOpen}>
                         <svg className="playlistartwork">
                             {/* <path d="M33.402 3.006L8.852 31.751l-2.337 12.61 12.09-4.281 24.552-28.746-9.755-8.328zM9.112 41.32l1.543-8.327 6.44 5.5-7.983 2.827zm9.418-4.231l-6.712-5.732L33.625 5.825l6.711 5.731L18.53 37.089z"></path> */}
                             <path fill="#afafaf" d="M25.6 11.565v45.38c-2.643-3.27-6.68-5.37-11.2-5.37-7.94 0-14.4 6.46-14.4 14.4s6.46 14.4 14.4 14.4 14.4-6.46 14.4-14.4v-51.82l48-10.205V47.2c-2.642-3.27-6.678-5.37-11.2-5.37-7.94 0-14.4 6.46-14.4 14.4s6.46 14.4 14.4 14.4S80 64.17 80 56.23V0L25.6 11.565zm-11.2 65.61c-6.176 0-11.2-5.025-11.2-11.2 0-6.177 5.024-11.2 11.2-11.2 6.176 0 11.2 5.023 11.2 11.2 0 6.174-5.026 11.2-11.2 11.2zm51.2-9.745c-6.176 0-11.2-5.024-11.2-11.2 0-6.174 5.024-11.2 11.2-11.2 6.176 0 11.2 5.026 11.2 11.2 0 6.178-5.026 11.2-11.2 11.2z"></path>
@@ -51,7 +51,7 @@ class PlaylistForm extends React.Component {
                         <div className="pldetail1">
                             PLAYLIST
                         </div>
-                        <div className="pldetailstitle">
+                        <div className="pldetailstitle" onClick={this.handlePLClickOpen}>
                             {this.state.title}
                         </div>
                         <div className="description">
@@ -78,8 +78,6 @@ class PlaylistForm extends React.Component {
                     {/* add song button instead for now */}
                 </div>
                 
-                <button id="myBtn" onClick={this.handlePLClickOpen}> Open Modal</button>
-                {/* {window.onclick = this.handlePLClickClose} */}
                 {/* everything below should be in a modal form */}
                 <div id="playlistModal" className="playlistModalyay">
                     <div className="playlistModalContent">
@@ -90,6 +88,11 @@ class PlaylistForm extends React.Component {
                                 <path fill="#afafaf" d="M14.354 2.353l-.708-.707L8 7.293 2.353 1.646l-.707.707L7.293 8l-5.647 5.646.707.708L8 8.707l5.646 5.647.708-.708L8.707 8z"></path>
                             </svg>
                         </button>
+                        <div className="plformartwork">
+                            <svg className="playlistartwork">
+                                <path fill="#afafaf" d="M25.6 11.565v45.38c-2.643-3.27-6.68-5.37-11.2-5.37-7.94 0-14.4 6.46-14.4 14.4s6.46 14.4 14.4 14.4 14.4-6.46 14.4-14.4v-51.82l48-10.205V47.2c-2.642-3.27-6.678-5.37-11.2-5.37-7.94 0-14.4 6.46-14.4 14.4s6.46 14.4 14.4 14.4S80 64.17 80 56.23V0L25.6 11.565zm-11.2 65.61c-6.176 0-11.2-5.025-11.2-11.2 0-6.177 5.024-11.2 11.2-11.2 6.176 0 11.2 5.023 11.2 11.2 0 6.174-5.026 11.2-11.2 11.2zm51.2-9.745c-6.176 0-11.2-5.024-11.2-11.2 0-6.174 5.024-11.2 11.2-11.2 6.176 0 11.2 5.026 11.2 11.2 0 6.178-5.026 11.2-11.2 11.2z"></path>
+                            </svg>
+                        </div>
                         <form onSubmit={this.handleSubmit} className="playlistForm">
                             <label id="plformlabel" htmlFor="pltitle"></label>
                             <input
@@ -122,7 +125,7 @@ class PlaylistForm extends React.Component {
                             <div className="savebtn">
                                 <button type='submit' className="savebutton">SAVE</button>
                             </div>
-                            <p>By proceeding, you agree to give CharmTunes access to the image you choose to upload. Please make sure you have the right to upload the image.</p>
+                            <p className="modalPara">By proceeding, you agree to give CharmTunes access to the image you choose to upload. Please make sure you have the right to upload the image.</p>
                         </form>
                     </div>
                 </div>
