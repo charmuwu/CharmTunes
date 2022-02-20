c046c576154c0f4f4520f9382bce98e3f058533f
Andrew Kim <andrewikim95@gmail.com>
2022-02-20 18:45:45 -0500

    add and adjust user back end routes

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,7 +1,12 @@
 class Api::UsersController < ApplicationController
 
     def index
+        @users = User.all
         render :index
+    end
+    def show
+        @user = User.find_by(id: params[:id])
+        render :show
     end
 
     def create 
