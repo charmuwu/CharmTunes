25e50c37ad13c65cfef5477c8f265ecdd04d2119
Andrew Kim <andrewikim95@gmail.com>
2022-02-23 01:07:01 -0500

    add playsong actions and placeholders

---
+++ frontend/actions/playsong_actions.js
@@ -0,0 +1,19 @@
+import { fetchPlaysongs, postPlaysong, deletePlaysong } from "../utils/playsong_api_util";
+
+export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+
+export const receiveErrors = (errors) => ({
+    type: RECEIVE_ERRORS,
+    errors,
+});
+
+export const getPlaysongs = playlistId => dispatch => fetchPlaysongs(playlistId)
+    .then( playsongs => dispatch({type: RECEIVE_PLAYSONGS, playsongs})); // will have to fix
+
+export const createPlaysong = (playlistId, songId) => dispatch => postPlaysong(playlistId, songId)
+    .then( playsong => dispatch({type: CREATE_PLAYSONG, playsong}), 
+        error => dispatch(receiveErrors(error.responseJSON)));
+    // will have to fix?
+
+export const deletePlaysong = (playlistId, songId) => dispatch => deletePlaysong(playlistId, songId)
+    .then (playsong => dispatch({type: DELETE_PLAYSONG, playsong})); // will have to fix ? 
