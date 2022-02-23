32a628d37528e7e5d56f220d37201d1db4696ca2
Andrew Kim <andrewikim95@gmail.com>
2022-02-22 21:12:01 -0500

    add in addsongscomponent for adding in songs to a playlist

---
+++ frontend/components/songs/add_songs_component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class AddSongs extends React.Component{
+    constructor(props){
+        super(props)
+        // this.handleClick = this.handleClick.bind(this);
+
+    }
+    render(){
+
+        return(
+            <div className="songAddContainer">
+
+            </div>
+        );
+    }
+}
+
+export default AddSongs;
--- frontend/components/songs/songs_container.jsx
+++ frontend/components/songs/songs_container.jsx
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux';
 import SongsComponent from './songs_component';
 import {getSongs, getSong} from '../../actions/music_actions';
+import AddSongs from './add_songs_component';
 
 const mSTP = state => ({
     songs: state.entities.songs
@@ -8,7 +9,9 @@ const mSTP = state => ({
 
 const mDTP = dispatch => ({
     getSong: songId => dispatch(getSong(songId)),
-    getSongs: () => dispatch(getSongs())
+    getSongs: () => dispatch(getSongs()),
+    //createPlaySong: () => dispatch(makePlaySong())
 });
 
 export default connect(mSTP,mDTP)(SongsComponent);
+// export default connect(mSTP,mDTP)(AddSongs);
