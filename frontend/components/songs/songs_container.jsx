dc93eaf01ebd1ec317c0485520b8b37c12c24b59
Andrew Kim <andrewikim95@gmail.com>
2022-02-25 00:08:05 -0500

    import and mDTP createPlaysong action

--- frontend/components/songs/songs_container.jsx
+++ frontend/components/songs/songs_container.jsx
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import SongsComponent from './songs_component';
 import AddSongs from './add_songs_component';
 import {getSongs, getSong} from '../../actions/music_actions';
-import {createPlaySong} from '../../actions/playsong_actions';
+import {createPlaysong} from '../../actions/playsong_actions';
 
 const mSTP = state => ({
     songs: state.entities.songs
@@ -11,7 +11,7 @@ const mSTP = state => ({
 const mDTP = dispatch => ({
     getSong: songId => dispatch(getSong(songId)),
     getSongs: () => dispatch(getSongs()),
-    createPlaySong: (playlistId, songId) => dispatch(createPlaySong(playlistId, songId)),
+    createPlaysong: (playlistId, songId) => dispatch(createPlaysong(playlistId, songId)),
 });
 
 export default {
